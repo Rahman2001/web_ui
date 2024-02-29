3e4f6dca9245f2dea7fdd73340c9ada692bff161
Rahman2001 <rahmanrejepov777@gmail.com>
2024-02-29 13:34:38 +0300

    updated main

--- src/main/java/com/online_shopping_system/web_ui/Main.java
+++
@@ -1,7 +0,0 @@
-package com.online_shopping_system.web_ui;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/online_shopping_system/web_ui/WebUIApplication.java
@@ -0,0 +1,13 @@
+package com.online_shopping_system.web_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebUIApplication.class, args);
+    }
+
+}
